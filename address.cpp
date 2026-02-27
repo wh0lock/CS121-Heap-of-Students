617382b18e9b7cd4143f2b1a23f0600129516712
Emily Adams <emilyalexisadams@gmail.com>
2026-02-27 11:10:16 -0500

    Address fix

--- address.cpp
+++ address.cpp
@@ -10,16 +10,10 @@ Address::Address(){
 } // end constructor
 
 void Address::init(std::string street, std::string city, std::string state, std::string zip){
-	// make stringstream 
-	std::stringstream converter;
-	// separate
-	converter.clear();
-	converter.str("");
-
-	getline(converter, street, ',');
-	getline(converter, city, ',');
-	getline(converter, state, ',');
-	getline(converter, zip, ',');
+	Address::street = street;
+	Address::city = city;
+	Address::state = state;
+	Address::zip = zip;
 } // end init
  
 void Address::printAddress(){
